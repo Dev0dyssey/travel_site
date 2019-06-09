2492d7bea673bb1bee09cf5408df3b7c18effc27
ThomasMartinUK <tmuk@protonmail.com>
2019-06-09 14:14:26 +0100

    Added dropdown menu to the <Navbar />

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -9,7 +9,19 @@ export default class navbar extends Component {
                 <img src={logo} alt="logo"></img>
             </span>
             <span className="navbar-brand">
-                <i class="fas fa-bars fa-2x"></i>
+                {/* <a className='nav-link' href="#">
+                <i class="fas fa-bars fa-2x" style={{color: 'white'}}></i>
+                </a> */}
+                <span className = "nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i className="fas fa-bars fa-2x" style={{color: 'white'}}></i>
+                    </a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a className="dropdown-item" href="#">Profile</a>
+                        <a className="dropdown-item" href="#">Book a Tour</a>
+                        <a className="dropdown-item" href="#">Settings</a>
+                    </div>
+                </span>
             </span>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{fontFamily: `'Poppins', 'sans-serif'`, fontWeight: '700'}}>
